32069fe4c1f8dfc559b14b0157f12c956b4e16d7
RoederArt <arthur_roeder@estudante.sesisenai.org.br>
2026-06-03 11:17:03 -0300

    doc - documentação do connect feita

--- infra/db/connect.php
+++ infra/db/connect.php
@@ -7,7 +7,7 @@
 
     $conn = new mysqli($host,$user,$pass,$db);//cria um caminho com o banco
 
-    if($conn->connect_error){//verifica se a conecção ocorreu com sucesso
+    if($conn->connect_error){//verifica se a conexão foi bem sucedida
         die("Erro na conexão!");
     }else{
         echo "<script>console.log('Banco conectado com sucesso!')</script>";
